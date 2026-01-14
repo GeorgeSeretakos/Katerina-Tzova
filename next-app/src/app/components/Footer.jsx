126c0d04a471b96cb60f7d913d2807fd6d49a73a
GeorgeSeretakos <g.seretakos@gmail.com>
2026-01-14 11:56:41 +0200

    Translation & Privacy Policy

--- next-app/src/app/components/Footer.jsx
+++ next-app/src/app/components/Footer.jsx
@@ -140,7 +140,7 @@ export default function Footer() {
           name="contact"
           method="POST"
           data-netlify="true"
-          netlify-honeypot="bot-field"
+          data-netlify-honeypot="bot-field"
           className="space-y-4"
         >
           <input type="hidden" name="form-name" value="contact" />
